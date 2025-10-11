fc6fd5fa9c01feac9ee7dc7c411a8fd8bbaff801
milisp <milisp@proton.me>
2025-10-11 08:22:50 -0400

    R: set updater false

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,11 @@ import { RouterProvider } from "react-router-dom";
 import { router } from "./router";
 import { ThemeProvider } from "@/components/common/ThemeProvider";
 import "./App.css";
-import { UpdateChecker } from "./components/UpdateChecker";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <ThemeProvider>
       <RouterProvider router={router} />
-      <UpdateChecker />
     </ThemeProvider>
   </React.StrictMode>,
 );
